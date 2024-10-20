3ff997d52d877ef12bfa6371e86b4c2e726f1198
BrandonAWong <105482178+BrandonAWong@users.noreply.github.com>
2024-10-20 03:36:35 -0700

    Cull

--- src/GuessBox.jsx
+++ src/GuessBox.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './GuessBox.css';
 
 function GuessBox({ items }) {
   const [filter, setFilter] = useState('');
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const wrappedRef = useRef(null);
 
   const getItemsByFilter = () => {
     if (!filter) {
@@ -11,41 +13,65 @@ function GuessBox({ items }) {
 
     return items.filter(item => item.name.toLowerCase().replace(/\W/g, '')
       .startsWith(filter.toLowerCase().replace(/\W/g, '')));
-  }
+  };
+
+  const handleOnChange = (e) => {
+    setFilter(e.target.value);
+    setShowSuggestions(true);
+  };
 
   const checkEnterPressed = (e) => {
     const filteredItems = getItemsByFilter();
     if (filteredItems.length > 0 && e.key === 'Enter') {
       submitGuess(filteredItems[0]);
     }
-  }
+  };
 
   const submitGuess = (item) => {
     items.splice(items.indexOf(item), 1);
     setFilter('');
+    // TODO: Callback to parent
+  };
+
+  const handleClickOutside = (e) => {
+    if(wrappedRef.current && !wrappedRef.current.contains(e.target)) {
+      setShowSuggestions(false);
+    }
   }
 
-  return ( <>
-      <div>
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, []);
+
+  return ( 
+    <>
+      <div ref={wrappedRef}>
         <input
           placeholder="Type an item name..."
-          onChange={(e) => setFilter(e.target.value)}
+          onChange={handleOnChange}
           onKeyDown={checkEnterPressed}
+          onFocus={() => setShowSuggestions(true)}
           value={filter}
         />
-        <ul>
-          {getItemsByFilter().map((item, index) => (
-            <li
-              key={index}
-              onClick={() => submitGuess(item)}
-            >
-              <div>
-                <img src={item.image} />
-                {item.name}
-              </div>
-            </li>
-          ))}
-        </ul>
+        {showSuggestions &&
+          <ul>
+            {getItemsByFilter().map((item, index) => (
+              <li
+                key={index}
+                onClick={() => submitGuess(item)}
+              >
+                <div>
+                  <img src={item.image} />
+                  {item.name}
+                </div>
+              </li>
+            ))}
+          </ul>
+        }
       </div>
     </>
   );
